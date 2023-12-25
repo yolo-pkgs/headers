17ed6d27bac12272af4aef28d82a04ff829e43a2
Gleb Buzin <qufiwefefwoyn@gmail.com>
2023-12-25 23:28:47 +0300

    fix: values

---
+++ values.go
@@ -0,0 +1,8 @@
+package headers
+
+const (
+	ContentTypeJSON              = "application/json"
+	ContentTypeNDJSON            = "application/x-ndjson"
+	ContentTypeWWWFormURLEncoded = "application/x-www-form-urlencoded"
+	ContentTypeMultipartFormData = "multipart/form-data"
+)
